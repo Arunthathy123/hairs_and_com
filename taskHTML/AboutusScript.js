a153804c346ae015fa7ec2c98c469a80740fe785
Arunthathy <arunthathy@glidix.com>
2025-03-11 09:09:10 +0530

    completed

--- taskHTML/AboutusScript.js
+++ taskHTML/AboutusScript.js
@@ -80,3 +80,16 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let currentPage = window.location.pathname.split("/").pop();
+
+    let navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach(link => {
+        if (link.getAttribute("href") === currentPage) {
+            link.classList.add("active");
+        }
+    });
+});
